9064be3384c8b2f30cbec3f9471f996e80c6e236
Natapfel <natanelapfel@gmail.com>
2020-04-06 23:40:30 +0100

    changed message

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ commandHandlerForCommandName['gamereq'] = (msg) => {
     if (bot.channel) {
         const message = msg.content.slice(9);
         const mention = msg.author.mention;
-        return bot.channel.createMessage(mention + ' has posted the following game request:' + '```css ' + message + '```' + 'If you are interested react to this post with emojis to let them know!!');
+        return bot.channel.createMessage(mention + ' has posted the following game request:' + '```' + message + '```' + 'If you are interested react to this post with emojis to let them know!!');
     }
 
     return msg.channel.createMessage('Error. Could not create message! Please notify Nat.');
